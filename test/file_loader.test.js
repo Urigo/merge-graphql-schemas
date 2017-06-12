52ee1492d16b4847ca08f922b2cd39f8433c4fd9
Guylian Cox <gc@pictawall.com>
2017-06-12 14:35:31 +0200

    feat: Add support for loading .graphql & .graphqls files
    This commit adds the possibility to write graphql schemas directly in .graphql files, instead of as
    the default exports of .js files

--- test/file_loader.test.js
+++ test/file_loader.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 import fileLoader from '../src/file_loader';
 import clientType from './graphql/types/client_type';
@@ -9,7 +10,12 @@ import personSearchType from './graphql/types/person_search_type';
 describe('fileLoader', () => {
   describe('with default options', () => {
     it('loads all files from specified folder', () => {
-      const types = [clientType, personEntityType, personSearchType, productType, vendorType];
+      const rawType = fs.readFileSync(`${__dirname}/graphql/types/raw_type.graphqls`).toString();
+
+      const types = [
+        clientType, personEntityType, personSearchType, productType, rawType, vendorType,
+      ];
+
       const loadedTypes = fileLoader(path.join(__dirname, 'graphql/types'));
 
       expect(loadedTypes).toEqual(types);
