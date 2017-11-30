1917349ddd3df24881730398d6ba65b0a3323876
Marcos Bergamo <marcos@thedon.com.br>
2017-11-30 19:52:03 +0100

    Fix indenting to 6 instead of 14 spaces

--- src/file_loader.js
+++ src/file_loader.js
@@ -48,35 +48,35 @@ const fileLoader = (folderPath,
   const schemafiles = getSchemaFiles(dir, recursive, globOptions);
 
   const files = schemafiles
-            .map(f => ({ f, pathObj: path.parse(f) }))
-            .filter(({ pathObj }) => pathObj.name.toLowerCase() !== 'index')
-            .filter(({ pathObj }) => extensions.includes(pathObj.ext))
-            .map(({ f, pathObj }) => {
-              let returnVal;
+        .map(f => ({ f, pathObj: path.parse(f) }))
+        .filter(({ pathObj }) => pathObj.name.toLowerCase() !== 'index')
+        .filter(({ pathObj }) => extensions.includes(pathObj.ext))
+        .map(({ f, pathObj }) => {
+          let returnVal;
 
-              switch (pathObj.ext) {
-                case '.ts':
-                case '.js': {
-                  const file = require(f); // eslint-disable-line
-                  returnVal = file.default || file;
-                  break;
-                }
+          switch (pathObj.ext) {
+            case '.ts':
+            case '.js': {
+              const file = require(f); // eslint-disable-line
+              returnVal = file.default || file;
+              break;
+            }
 
-                case '.graphqls':
-                case '.gql':
-                case '.graphql': {
-                  const file = fs.readFileSync(f, 'utf8');
-                  returnVal = file;
-                  break;
-                }
+            case '.graphqls':
+            case '.gql':
+            case '.graphql': {
+              const file = fs.readFileSync(f, 'utf8');
+              returnVal = file;
+              break;
+            }
 
-                default:
-                    // we don't know how to handle other extensions
-              }
+            default:
+                // we don't know how to handle other extensions
+          }
 
-              return returnVal;
-            })
-            .filter(v => !!v);    // filter files that we don't know how to handle
+          return returnVal;
+        })
+        .filter(v => !!v);    // filter files that we don't know how to handle
 
   return files;
 };
