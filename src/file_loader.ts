bdaf07001f230add6bee189e4d5a7c8282e2511c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-07-23 23:52:21 -0400

    Fix fileLoader signature #210

--- src/file_loader.ts
+++ src/file_loader.ts
@@ -6,7 +6,7 @@ const fileLoader = (
   path: string,
   { recursive = false, extensions = DEFAULT_EXTENSIONS, globOptions = {},
     ignoreIndex = true, } = {},
-) => {
+): any[] => {
   return loadSchemaFiles(path, {
       globOptions,
       extensions: extensions.map(e => e.startsWith('.') ? e.substr(1, e.length - 1) : e).filter(e => DEFAULT_EXTENSIONS.includes(e)),
