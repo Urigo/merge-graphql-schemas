0c4a09b30a8fbeeb86905268fea58b6d819ed181
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-12-31 14:41:00 +0300

    Use loadFiles

--- src/file_loader.ts
+++ src/file_loader.ts
@@ -1,9 +1,9 @@
-import { loadSchemaFiles } from '@graphql-toolkit/file-loading';
+import { loadFiles } from '@graphql-toolkit/file-loading';
 
 const DEFAULT_EXTENSIONS = ['ts', 'js', 'gql', 'graphql', 'graphqls'];
 const DEFAULT_IGNORED_EXTENSIONS = ['spec', 'test', 'd', 'map'];
 
-export const fileLoader = (
+export const fileLoader = <T = any>(
   path: string,
   { 
     recursive = false, 
@@ -12,8 +12,8 @@ export const fileLoader = (
     ignoreIndex = true, 
     ignoredExtensions = DEFAULT_IGNORED_EXTENSIONS,
   } = {},
-): any[] => {
-  return loadSchemaFiles(path, {
+): T[] => {
+  return loadFiles(path, {
       globOptions,
       extensions: extensions.map(e => e.startsWith('.') ? e.substr(1, e.length - 1) : e).filter(e => DEFAULT_EXTENSIONS.includes(e)),
       recursive,
